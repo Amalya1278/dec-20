311ade269250e82326727b78287df96ef7091f60
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-12-28 16:03:31 +0400

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
+
 const logging=(req,res,next)=>{
     let time=Date.now()
     console.log(`[${time}] Method:${req.method} URL:${req.url}`)
     next()
     
 }
+
 app.use(logging)
  app.get('/users',(req,res)=>{
      res.send("get")
